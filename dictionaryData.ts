97b3e057a0023c6e01b017616f9aec8b44709439
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-07-26 20:47:34 +0200

    Update dictionaryData.ts

--- dictionaryData.ts
+++ dictionaryData.ts
@@ -2095,3 +2095,69 @@ instruire.
 ázírtabîlgan adj. disponibil. 
 ázírtabîlma s. disponibilitate; 
 prezenŃă.
+jagun I. num. (mong.) sută. II. s. (mil.) 
+escadron; companie; centurie. 
+jagunbaşî s. (mil.) căpitan. 
+jagwar s. (zool.) jaguar (lat., 
+Panthera on ca). 
+jale s. (meteo.) rouă. 
+jalelengen s. înrourat. 
+jalelenme s. înrourare. 
+jalelenmek v.i. a se înroura. 
+jalelí adj. înrourat. 
+Jamayka s. (topon.) Jamaica. 
+jamaykalî s., adj. jamaican. 
+jandarma s. 1. jandarmerie. 2. 
+jandarm. 
+japon s., adj. japonez. 
+Japon s. (topon.) Japonia. 
+Japon-akkózlíg í s. (orn.) 
+privighetoare-cu-ochi-alb japoneză 
+(lat., Zosterops japonicus). 
+Japon-bódenesí s. (orn.) prepeliŃă 
+japoneză (lat., Coturnix 
+japonica). 
+japonğa I. adv. în limba japoneză. II. 
+s. (limba) japoneză. 
+Japon-kurmas î s. (bot.) curmal-
+japonez; abanos; eben (lat., 
+Dyospiros kaki). 
+Japon-lád iní s. (bot.) brad japonez 
+(lat., Abies f irma). 
+Japon-múyúzbaşî s. (orn.) mătăsar 
+japonez (lat., Bo mbycilla 
+japonica). 
+Japon-narusî s. (bot.) brad japonez 
+(lat., Abies f irma). 
+Japon-salkîmî s. (bot.) salcâm-
+japonez; soforă (lat., Sophora 
+japonica). 
+Japon-sam îrî s. (orn.) mătăsar 
+japonez (lat., Bo mbycilla 
+japonica). 
+Japon-şîrşîsî s. (bot.) brad japonez 
+(lat., Abies f irma). 
+Japon-yúpekkuyrugî s. (orn.) 
+mătăsar japonez (lat., Bombycilla 
+japonica). 
+jar num. (mong.) şaizeci. 
+jargon s. (lingv.) jargon. 
+jartiyera s. jartieră. 
+jelew s. (gastron.) jeleu. 
+jersew s. jerseu. 
+jes-us s., adj. (ist.) sakait. 
+jeton s. jeton. 
+jidgelek s. (bot.) jujubă (lat., 
+Ziziphus jujuba). 
+jiruga num. (mong.) şase. 
+jojoba s. (bot.) jojoba (lat., 
+Simmondsia c hinesis). 
+jubilew s. jubileu. 
+jubur s. (geogr.) vale; luncă; păşune. 
+judo s. (sport) judo. 
+junta s. (pol.) juntă. 
+juriy s. juriu. 
+jurnal s. jurnal. 
+jurnalist s. jurnalist. 
+jurnalistika s. jurnalistică. 
+jurnalistlík s. jurnalistică.
