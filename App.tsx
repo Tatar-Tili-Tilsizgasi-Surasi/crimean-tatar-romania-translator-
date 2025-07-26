43d9da0e4c8529a7b16c35971dce40e7a0a68dc0
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-07-26 20:08:41 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -196,7 +196,7 @@ const App: React.FC = () => {
         </div>
       </main>
       <footer className="text-center text-slate-500 mt-10 text-xs sm:text-sm">
-        Powered by Google Gemini API. UI by AI.
+        Tatar Tílí Tílsîzgasî Şurasî<br>Powered by Google Gemini API. UI by AI.</br>
       </footer>
     </div>
   );
