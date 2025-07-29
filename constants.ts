5874a8d05d3b3f57b5b7737f237d9667a763d434
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-07-29 20:20:14 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,10 +3,11 @@ import { LanguageOption } from './types';
 
 export const LANGUAGES: LanguageOption[] = [
   { code: 'crh-RO', name: 'Crimean Tatar (Romania)' },
-  { code: 'crh', name: 'Crimean Tatar' },
+  { code: 'en', name: 'English' },
+  { code: 'ro', name: 'Romanian'},
 ];
 
-export const DEFAULT_SOURCE_LANG = 'crh';
-export const DEFAULT_TARGET_LANG = 'crh-RO';
+export const DEFAULT_SOURCE_LANG = 'crh-RO';
+export const DEFAULT_TARGET_LANG = 'en';
 
 export const GEMINI_MODEL_NAME = 'gemini-2.5-flash';
