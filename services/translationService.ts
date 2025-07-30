72ef89cdad27d0af3d2ea6f62192b76fd3a0f389
Tatar-Tili-Tilsizgasi-Surasi <tatartilintilsizgasurasi@gmail.com>
2025-07-30 15:30:26 +0200

    Update translationService.ts

--- services/translationService.ts
+++ services/translationService.ts
@@ -1,5 +1,6 @@
 import { GoogleGenAI, GenerateContentResponse } from "@google/genai";
 import { GEMINI_MODEL_NAME } from '../constants';
+import { PARSED_DICTIONARY_ENTRIES } from '../dictionaryData';
 import {
   CRIMEAN_TATAR_RO_ALPHABET_PRONUNCIATION_GUIDE,
   CRIMEAN_TATAR_RO_ORTHOGRAPHY_INFO,
